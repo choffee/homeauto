c4e048c71e02f23764a38b90ca0ef2712eb3b13c
John Cooper <john@choffee.co.uk>
2013-08-11 22:12:42 +0100

    gofmt power.go

--- power.go
+++ power.go
@@ -1,28 +1,26 @@
 package main
 
 import (
+	"bitbucket.org/shanehanna/mosquitto"
 	"github.com/tarm/goserial"
-  "bitbucket.org/shanehanna/mosquitto"
-	"log"
 	"io"
-  "regexp"
+	"log"
+	"regexp"
 )
 
-
-func serialReader(serialPort *io.ReadWriteCloser , line *chan<- *string ) {
+func serialReader(serialPort *io.ReadWriteCloser, line *chan<- *string) {
 	// Reads continuisly from a serial port and sends whole line back
 	buf := make([]byte, 128)
 	for {
 		n, err := (*serialPort).Read(buf)
 		if err != nil {
-      log.Fatalf("Error reading from serial port: %v", err)
-      //break
+			log.Fatalf("Error reading from serial port: %v", err)
+			//break
 		}
 		log.Printf("%q", buf[n])
-        }
+	}
 
 }
-
 
 func main() {
 	c := &serial.Config{Name: "/dev/ttyUSB1", Baud: 9600}
@@ -35,35 +33,35 @@ func main() {
 
 	go serialReader(&s, serialLines)
 
-  // Now setup the mqtt connection
-  conn, _ := mosquitto.Dial("example", "192.168.1.105:1883", true)
-  go conn.Listen()
+	// Now setup the mqtt connection
+	conn, _ := mosquitto.Dial("example", "192.168.1.105:1883", true)
+	go conn.Listen()
 
-  device_re, err := regexp.Compile(`/homeauto/power/([^/]+)`)
-  if err != nil {
-    log.Fatal(err)
-  }
-  err = conn.HandleFunc("/homeauto/power/#", 2, func(c *mosquitto.Conn, m mosquitto.Message) {
-    log.Printf("foo <- (%s)\nfoo -> bar(%s)\n", m.Payload, m.Payload)
-    rec := device_re.FindStringSubmatch(m.Topic)
-    log.Printf("%v", rec[1])
-    // Check the length of rec here XXX
-    device := rec[1]
-    if ( string(m.Payload) == "on" || string(m.Payload) == "off" ) {
-      msg := "RF  " + device + string(m.Payload) + "\n"
-      _, err = s.Write([]byte(msg))
-      if err != nil {
-        log.Fatal(err)
-      }
-    }
-  })
+	device_re, err := regexp.Compile(`/homeauto/power/([^/]+)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = conn.HandleFunc("/homeauto/power/#", 2, func(c *mosquitto.Conn, m mosquitto.Message) {
+		log.Printf("foo <- (%s)\nfoo -> bar(%s)\n", m.Payload, m.Payload)
+		rec := device_re.FindStringSubmatch(m.Topic)
+		log.Printf("%v", rec[1])
+		// Check the length of rec here XXX
+		device := rec[1]
+		if string(m.Payload) == "on" || string(m.Payload) == "off" {
+			msg := "RF  " + device + string(m.Payload) + "\n"
+			_, err = s.Write([]byte(msg))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	})
 
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  // Just wait at the end
-  end := make(chan bool, 1)
-  <- end
+	// Just wait at the end
+	end := make(chan bool, 1)
+	<-end
 
 }
